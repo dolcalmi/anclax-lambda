fad940e1291761278a954566aa9901c280b93402
Adolfo Builes <builes.adolfo@gmail.com>
2018-03-22 10:40:42 -0500

    Cleanup payments function.

--- functions/payment/src/index.js
+++ functions/payment/src/index.js
@@ -7,8 +7,8 @@ exports.handle = function(e, ctx, cb) {
   var receiverPublicKey = e.receiverPublicKey
   var amount = e.amount
 
-  var sourceKeypair = StellarSdk.Keypair.fromSecret(sourceSecretKey);
-  var sourcePublicKey = sourceKeypair.publicKey();
+  var sourceKeypair = StellarSdk.Keypair.fromSecret(sourceSecretKey)
+  var sourcePublicKey = sourceKeypair.publicKey()
 
   server.loadAccount(sourcePublicKey)
     .then(function(account) {
@@ -18,24 +18,24 @@ exports.handle = function(e, ctx, cb) {
             asset,
             amount
           }))
-          .build();
+          .build()
 
-      transaction.sign(sourceKeypair);
+      transaction.sign(sourceKeypair)
 
       server.submitTransaction(transaction)
         .then(function(transactionResult) {
-          console.log(JSON.stringify(transactionResult, null, 2));
+          console.log(JSON.stringify(transactionResult, null, 2))
 
           cb(null, transactionResult)
         })
         .catch(function(err) {
-          console.log(err);
+          console.log(err)
 
           cb(JSON.stringify(err))
-        });
+        })
     })
     .catch(function(e) {
-      console.error(e);
-      cb("err loading account")
-    });
+      console.error(e)
+      cb(JSON.stringify(e))
+    })
 }
