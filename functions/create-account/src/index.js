1e75351fc9149438512353767c10c84f57a96eed
Adolfo Builes <builes.adolfo@gmail.com>
2018-03-22 12:21:27 -0500

    Use async/await in create account

--- functions/create-account/src/index.js
+++ functions/create-account/src/index.js
@@ -1,6 +1,17 @@
+// Call locally with .default({}, {}, (a, b) => { console.log(b) })
 import apex from 'apex.js'
+import StellarSdk  from 'stellar-sdk'
+
+import server from '../../../lib/server'
+
+export async function createAccount(e) {
+  const issuingKeys = StellarSdk.Keypair.fromSecret('SBQWY3DNPFWGSZTFNV4WQZLBOJ2GQYLTMJSWK3TTMVQXEY3INFXGO52X')
+
+  const account = await server.loadAccount(issuingKeys.publicKey())
+
+  return { account }
+}
 
 export default apex(e => {
-  console.log('processing event: %j', e)
-  return { hello: 'world' }
+  return createAccount(e);
 })
