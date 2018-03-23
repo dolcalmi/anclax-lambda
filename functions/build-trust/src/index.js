579a09b86288247234c16f0c5ccf68b61709ce23
Adolfo Builes <builes.adolfo@gmail.com>
2018-03-22 19:40:28 -0500

    Double trust.

--- functions/build-trust/src/index.js
+++ functions/build-trust/src/index.js
@@ -7,6 +7,7 @@ import asset from '../../../lib/asset'
 
 const paymentsPkey = process.env['PAYMENTS_PUBKEY']
 const adminPkey = process.env['ADMIN_PUBKEY']
+const authSeed = process.env['AUTH_SEED']
 
 export async function buildTrust({ seed }) {
   var trusterKeys = StellarSdk.Keypair.fromSecret(seed);
@@ -44,7 +45,26 @@ export async function buildTrust({ seed }) {
 
   transaction.sign(trusterKeys)
 
-  const result = await server.submitTransaction(transaction)
+  await server.submitTransaction(transaction)
+
+  // now generate trustline from anchor to account
+  const issuingKeys = StellarSdk.Keypair.fromSecret(authSeed)
+
+  const issuingAccount = await server.loadAccount(issuingKeys.publicKey())
+
+  const transaction2 = new StellarSdk.TransactionBuilder(issuingAccount)
+      .addOperation(
+        StellarSdk.Operation.allowTrust({
+          trustor: trusterKeys.publicKey(),
+          assetCode: asset.code,
+          authorize: true,
+          source: asset.issuer
+        })
+      )
+      .build();
+
+  transaction2.sign(issuingKeys);
+  const result = await server.submitTransaction(transaction2);
 
   return { result }
 }
