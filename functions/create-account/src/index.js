91598974aab887a1fab06307b6ff285a78f18333
Adolfo Builes <builes.adolfo@gmail.com>
2018-03-24 14:37:42 -0500

    Connect create-account with build-trust.

--- functions/create-account/src/index.js
+++ functions/create-account/src/index.js
@@ -5,19 +5,56 @@ import StellarSdk  from 'stellar-sdk'
 import extractSnsMessage from '../../../lib/extract-sns-message'
 import server from '../../../lib/server'
 import sns from '../../../lib/sns'
+import decrypt from '../../../lib/decrypt'
 
-export async function createAccount({ destination }, { awsRequestId }) {
+async function sendToSns(params) {
+  await sns.publish(params)
+}
+
+async function sendMessageToSlack(text) {
+  try {
+    await sendToSns({
+      Message: JSON.stringify({
+        text
+      }),
+      TopicArn: process.env.SlackTopicArn
+    })
+
+  } catch(e) {
+    console.log('slack-notification-failed')
+  }
+}
+
+async function buildTrust(secret, newAccount, awsRequestId) {
+  try {
+    await sendToSns({
+      Message: JSON.stringify({
+        secret
+      }),
+      TopicArn: process.env.BuildTrustTopicArn
+    })
+  } catch (e) {
+    sendMessageToSlack(`<!everyone> :warning: sns-build-trust failed ${newAccount.publicKey()} - Lambda ID: ${awsRequestId}`)
+
+    throw e
+  }
+}
+
+export async function createAccount({ secret }, { awsRequestId }) {
+  const accountSecret = await decrypt(secret)
+
   const issuingKeys = StellarSdk.Keypair.fromSecret(process.env.IssuingKeys)
-
   const issuingAccount = await server.loadAccount(issuingKeys.publicKey())
 
-  console.log('creating account', destination)
+  const newAccount = StellarSdk.Keypair.fromSecret(accountSecret)
+
+  console.log('creating account', newAccount.publicKey())
 
   const transaction = new StellarSdk.
         TransactionBuilder(issuingAccount)
         .addOperation(
           StellarSdk.Operation.createAccount({
-            destination,
+            destination: newAccount.publicKey(),
             startingBalance: '2.6'
           })
         ).build()
@@ -27,18 +64,16 @@ export async function createAccount({ destination }, { awsRequestId }) {
   try {
     const result = await server.submitTransaction(transaction);
 
-    const snsResult = await sns.publish({
-      Message: JSON.stringify({
-        text: `<!here> :+1: New account created ${destination}`
-      }),
-      TopicArn: process.env.SlackTopicArn
-    })
+    await Promise.all([
+      buildTrust(secret, newAccount, awsRequestId),
+      sendMessageToSlack(`<!here> :+1: New account created ${newAccount.publicKey()}`)
+    ])
 
-    return { result, snsResult }
+    return { result }
   } catch (e) {
     await sns.publish({
       Message: JSON.stringify({
-        text: `<!everyone> :warning: Account creation failed ${destination} - Lambda ID: ${awsRequestId}`
+        text: `<!everyone> :warning: Account creation failed ${newAccount.publicKey()} - Lambda ID: ${awsRequestId}`
       }),
       TopicArn: process.env.SlackTopicArn
     })
